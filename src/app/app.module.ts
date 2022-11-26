04c5b57807f99928555356f05f396e73759cdde5
Maestro <mica@morph.fi>
2022-11-26 20:20:56 +0200

    update display-deteils component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,9 +12,8 @@ import { DisplayDetailsComponent } from './display-details/display-details.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+    AppRoutingModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/display-details/display-details.component.ts
+++ src/app/display-details/display-details.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-display-details',
   templateUrl: './display-details.component.html',
   styleUrls: ['./display-details.component.css']
 })
+
 export class DisplayDetailsComponent implements OnInit {
+  isShown = false;
+  timeStamps = [] as any;
+  timeStamp: number = 0;
+  current: string = new Date().toUTCString();
+  
+  constructor() { }
+  
+  ngOnInit(): void { }
+
+  onClick() {
+    this.isShown = !this.isShown;
+    this.timeStamp = this.timeStamps.length + 1;
+    this.timeStamps.push(this.current + this.timeStamp);
+  }
+}
+
+/* EKA YRITYS:
+  export class DisplayDetailsComponent implements OnInit {
   isShown = false;
   timeStampName = '1';
   timeStampAdded = false;
@@ -22,4 +42,4 @@ export class DisplayDetailsComponent implements OnInit {
     this.timeStampAdded = true;
     this.timeStamps.push(this.timeStampName);
   }
-}
+}  */
